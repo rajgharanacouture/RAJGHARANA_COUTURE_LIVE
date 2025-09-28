b9644a58c88f0e66c7249605f8ebe8d39da042e0
prateekgupta0308 <122146594+prateekgupta0308@users.noreply.github.com>
2025-09-28 15:43:44 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -158,6 +158,20 @@ document.addEventListener('DOMContentLoaded', function() {
     initializeLazyLoading();
 });
 
+// Show alert message
+function showAlert(message) {
+    //document.getElementById("showToast").addEventListener("click", () => {
+      let toastElement = document.getElementById("liveToast");
+      toastElement.innerHTML = `<div class="d-flex">
+                <div class="toast-body">
+                    ${message}
+                </div>
+            </div>`;
+      const toast = new bootstrap.Toast(toastElement);
+      toast.show();
+    //});
+}
+
 // Initialize carousels function
 function initializeCarousels() {
     // This function can be used to initialize any carousel functionality
@@ -219,20 +233,36 @@ function animateOnScroll() {
         observer.observe(element);
     });
 }
-//prateek
-function viewProduct(productId) {
-    // Populate modal
-    console.log("viewProduct called");
 
+function viewProduct(productName, productImage, productPrice, productDescription, productId) {
+    
+
+    //productJSON = JSON.parse(productJSON);
+    // Populate modal
+    
+    console.log( "viewProduct called",window.cartData);
     //document.getElementById('productModal').style.display = "block";
     //.textContent = product.name;
-    document.getElementById('productModalImage').src = product.image;
-    document.getElementById('productModalName').textContent = product.name;
-    document.getElementById('productModalCategory').textContent = product.category.charAt(0).toUpperCase() + product.category.slice(1);
-    document.getElementById('productModalPrice').textContent = `$${product.price}`;
-    document.getElementById('productModalDescription').textContent = product.description;
+    document.getElementById('productModalImage').src = productImage;
+    document.getElementById('productModalTitle').textContent = productName;
+    //document.getElementById('productModalCategory').textContent = 'WoMen';
+
+    const cartItem = window.cartData.find(cart => cart.productId == productId);
+    if (cartItem) {
+        document.getElementById('productQuantity').value = cartItem.quantity;
+    }
+
+    //document.getElementById('productQuantity').value = window.cartData[0].quantity;
+    document.getElementById('productModalPrice').textContent = '₹' + productPrice;
+    document.getElementById('productModalDescription').textContent = productDescription;
+    document.getElementById('addToCartBtn').setAttribute('onclick', "addToCart('" + productId  +"', document.getElementById('productQuantity').value, 'update');");
+
+    console.log(' #addToCartBtn ',document.getElementById('addToCartBtn'));
     
-    // Update size options
+
+ 
+
+    /*Update size options
     const sizeSelect = document.getElementById('productSize');
     sizeSelect.innerHTML = '';
     product.sizes.forEach(size => {
@@ -240,7 +270,7 @@ function viewProduct(productId) {
         option.value = size;
         option.textContent = size === 'One Size' ? 'One Size' : size;
         sizeSelect.appendChild(option);
-    });
+    });*/
 }
 
 // Modal switching
@@ -250,7 +280,14 @@ function viewProduct(productId) {
         const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
         registerModal.show();
     });
-//prateek
+
+
+ document.getElementById('showLogin').addEventListener('click', function(e) {
+        e.preventDefault();
+        bootstrap.Modal.getInstance(document.getElementById('registerModal')).hide();
+        const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+        loginModal.show();
+    });
 
 // Initialize animations when DOM is loaded
 document.addEventListener('DOMContentLoaded', animateOnScroll);
---
+++ js/server.js
@@ -0,0 +1,280 @@
+// Main JavaScript functionality for Raj Gharana website
+
+//document.addEventListener('DOMContentLoaded', function() {
+
+    window.cartData = [];
+    console.log('server js loaded ');
+    const supabaseUrl = "https://dklcbcbgpdrqsqupaaeb.supabase.co";
+
+
+    const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbGNiY2JncGRycXNxdXBhYWViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NzI1ODk1MiwiZXhwIjoyMDcyODM0OTUyfQ.EN8lHW9BBf1qSNXw9fFnFU1rWgIoOsWqltks57qp-pY"; // apna anon key lagao
+    const client = supabase.createClient(supabaseUrl, supabaseKey);
+
+        async function loadProducts() {
+            console.log('loadProducts called');
+            const { data, error } = await client.from("Products").select("*");
+
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            let listDiv = document.getElementsByClassName("what-wear");
+            console.log( JSON.stringify(listDiv) ,'<==listDiv==>',listDiv);
+            let products = '';      
+
+            data.forEach(product => {
+                
+                products += `<div class="card col-6" id="${product.id}">
+                                    <div class="card-header" >
+                                        <img src="${product.productimage}" onclick="viewProduct('${product.productname}', '${product.productimage}', '${product.productprice}', '${product.productdescription}', '${product.id}')" 
+                                        data-bs-toggle="modal" data-bs-target="#productModal" width="100%" height="200" alt="Product 1">
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="product-text">${product.productname}</p>
+                                        <p>₹${product.productprice}</p>
+                                        <div class="product-actions">
+                                            <button class="btn btn-view" onclick="addToCart('${product.id}', 1, 'add');" >
+                                                <i class="fas fa-cart-plus"></i> Add to Cart
+                                            </button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>`;
+                
+            });
+
+            listDiv[0].innerHTML = products;
+
+            console.log('data ',data);
+            //console.log('listDiv.innerHTML ',listDiv[0].innerHTML);
+        }
+
+        loadProducts();
+        loadCarts();
+
+        async function addToCart(productId, quantity, action){
+            console.log(' addToCart called ',productId);
+
+            let updateCart = { productId: productId, quantity : quantity };
+
+            const cartItem = cartData.find(cart => cart.productId == productId);
+            if (cartItem) {
+                updateCart.cart_id = cartItem.cart_id;
+                if(action == 'add'){
+                    updateCart.quantity = cartItem.quantity + 1;
+                }
+            }
+
+            let cartResponse = await client
+                .from("Cart")
+                .upsert([ updateCart ]);
+            
+
+            //let { data, error } = await client.from('Cart').select('cart_id').in('productId', [productId]);   
+
+             
+
+            
+
+            if (cartResponse.error) {
+                console.error(cartResponse.error);
+                showAlert("Error inserting card!");
+            } else {
+                console.log('data ',cartResponse.data);
+                showAlert('✨ Great choice! Your product is in the cart.');
+                //document.getElementById("sizeInput").value = "";
+                //document.getElementById("quantityInput").value = "";
+                loadCarts();
+            }
+        }
+
+        async function loadCarts() {
+            const cartResponse = await client.from("Cart").select("*").order("created_at", { ascending: false });
+            cartData = cartResponse.data;
+            let cartError = cartResponse.error;
+            if (cartError) { console.error(cartError); return; }
+
+            console.log('loadCarts called' , cartData);
+
+            let cartCount = document.getElementById('cartCount');
+            cartCount.style.display = cartData && cartData.length > 0 ? 'unset' : 'none';
+            cartCount.textContent = '' + cartData.length;
+            
+            /*const list = document.getElementById("cardList");
+            list.innerHTML = "";
+            data.forEach(c => {
+                list.innerHTML += `
+                <div class="card">
+                    <span>🃏 Size: ${c.size} | Qty: ${c.quantity} <small>(${c.created_at})</small></span>
+                    <button onclick="deleteCard(${c.id})">Delete</button>
+                </div>`;
+            });*/       
+            let productIds = [];
+            cartData.forEach(cart => {
+                productIds.push(cart.productId);
+            });
+            
+            let { data, error } = await client.from('Products').select('*').in('id', productIds)   
+
+            if (error) {
+                console.error('Error fetching products:', error)
+                return
+            }
+
+            console.log('Products:', data)
+
+            //data = JSON.parse( JSON.stringify(data));
+            for (let index = 0; index < data.length; index++) {
+                let dataElement = data[index];
+                for (let cartIndex = 0; cartIndex < cartData.length; cartIndex++) {
+                    const cartElement = cartData[cartIndex];
+                    if(dataElement.id == cartElement.productId){
+                        data[index].quantity = cartElement.quantity;
+                    }
+                }
+            }
+
+            const cartItems = document.getElementById('cartItems');
+
+            /*cartItems.innerHTML = data.map(item => `
+            <div class="cart-item">
+                <img src="${item.productimage}" alt="${item.productname}" class="cart-item-image">
+                <div class="cart-item-details">
+                    <div class="cart-item-name">${item.productname}</div>
+                    <div class="cart-item-price">₹${item.productprice}</div>
+                </div>
+                <div class="cart-item-controls">
+                    <button class="quantity-btn" onclick="updateQuantity(${item.productid}, -1)">-</button>
+                    <span class="mx-2">${item.quantity}</span>
+                    <button class="quantity-btn" onclick="updateQuantity(${item.productid}, 1)">+</button>
+                    <button class="remove-btn" onclick="removeFromCart(${item.productid})">Remove</button>
+                </div>
+            </div>
+            `).join('');*/
+
+            cartItems.innerHTML = `<!-- Mobile Card Layout -->
+                <div class="container d-block d-md-none">` + data.map(item =>`
+                        <div class="card mb-3">
+                            <img src="${item.productimage}" class="card-img-top img-thumbnail border-bottom" alt="Product image">
+                            <div class="card-body p-4">
+                                <h5 class="border-bottom"> <strong>${item.productname}</strong> </h5>
+                                <p class="border-bottom"><span class="fw-medium">Price:</span> ₹${item.productprice}</p>
+                                <p class="border-bottom"><span class="fw-medium">Quantity:</span> <input type="number" class="text-center" style="width:20%" value="${item.quantity}" min="1"></p>
+                                <p class="border-bottom"><span class="fw-medium">Subtotal:</span> ₹${item.quantity * item.productprice}</p>
+                            </div>
+                        </div>
+                    `).join('') + `</div> 
+                    <!-- Desktop Table Layout -->
+                    <div class="container d-none d-md-block">
+                    <table class="table table-sm table-hover align-middle">
+                        <thead>
+                        <tr>
+                            <th>Preview</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                    ` +  data.map(item =>  `<tr>
+                            <td>
+                            <img src="${item.productimage}" class="img-fluid" style="width:100px;" alt="Product image"><br>
+                            </td>
+                            <td>${item.productname} </td>
+                            <td>₹${item.productprice}</td>
+                            <td>
+                            <input type="number" value="${item.quantity}" class="form-control text-center" style="width:80px;">
+                            </td>
+                            <td>₹${item.quantity * item.productprice}</td>
+                        </tr>`).join('') + `</tbody>
+                    </table>
+                    </div>`;
+
+        
+        const total = data.reduce((sum, item) => sum + (item.productprice * item.quantity), 0);
+        document.getElementById('cartTotal').textContent = total.toFixed(2);
+
+        }
+    
+//prateek
+
+    async function signIn() {
+    
+        console.log('data called');
+        const email = document.getElementById("loginEmail").value.trim();
+        
+        const password = document.getElementById("loginPassword").value.trim();
+
+        if (!email || !password) {
+        showAlert("Kindly enter your email and password!");
+        return;
+        }
+
+        // Check user in DB
+        const { data, error } = await client
+        .from("Users")
+        .select("user_id, Name, Email, password")
+        .eq("Email", email)
+        .eq("password", password)  
+        
+
+        
+        
+        console.log('signin data, ',data);
+        
+
+        if (error || data.length == 0) {
+        console.error(error);
+        showAlert("Sign-in unsuccessful. Please verify your details.!");
+        return;
+        }
+
+        if (data.length > 0) {
+        // user found
+        const user = data[0];
+        //window.currentUserId = user.user_id; // Save logged-in user id
+        showAlert(`Login successful! Welcome ${user.Name}`);
+        } else {
+        showAlert("Invalid email or password!");
+        }
+    }
+
+    async function signUp() {
+
+        
+
+        const name = document.getElementById("registerFirstName").value.trim();
+        const lastName = document.getElementById("registerLastName").value.trim();
+        const email = document.getElementById("registerEmail").value.trim();
+        const phoneNumber = document.getElementById("registerPhoneNumber").value.trim();
+        const password = document.getElementById("registerPassword").value; // raw password (testing only)
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+    
+        console.log("name",name,"lastName", lastName,"email",email, "phoneNumber",phoneNumber,"password", password)
+        if (!name || !lastName || !email || !phoneNumber || !password || !confirmPassword) {
+            showAlert("Please fill all fields!");
+            return;
+        }
+        
+
+            // Validation
+        if (password !== confirmPassword) {
+            showAlert('Passwords do not match!', 'danger');
+            return;
+        }
+
+        if (password.length < 6) {
+            showAlert('Password must be at least 6 characters long!', 'danger');
+            return;
+        }
+
+
+      let signUpResponse = await client.auth.signUp({ email, password });
+      console.log(' signUpResponse ', signUpResponse);
+    }
+      
+
+//});
